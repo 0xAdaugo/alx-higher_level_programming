4bb05fca038defc20a8f9871c4fa9087a77dc55f
0xAdaugo <onuecheghekwuadaugo@gmail.com>
2023-08-28 18:40:23 -0700

    just trying to fic bugs

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
-
 def safe_print_division(a, b):
-    division_result = None  # Initialize the division result variable
+    response = None
     try:
-        division_result = a / b  # Try to perform the division
-    except:  # If there's an error, like division by zero
-        pass  # Ignore the error and continue
+        response = a / b
+    except:
+        pass
     finally:
-        print("Calculated result: {}".format(division_result))  # Print the result, even if it's None
-    return division_result  # Return the division result, even if it's None
-
+        print("Inside result: {}".format(response))
+    return response
