da5216099771667359f797cadbe204bef1da1f5f
0xAdaugo <onuecheghekwuadaugo@gmail.com>
2023-08-28 18:43:42 -0700

    pls work

--- 0x05-python-exceptions/5-raise_exception.py
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
-
 def raise_exception():
-    raise CustomTypeError("Custom exception raised")  # Raise a custom exception
-
-class CustomTypeError(Exception):
-    pass  # Define a custom exception class
-
+    raise TypeError("Exception raised")
--- 0x05-python-exceptions/6-raise_exception_msg.py
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
-
-def raise_exception_msg(custom_message=""):
-    raise CustomNameError(custom_message)  # Raise a custom error with a given message
-
-class CustomNameError(NameError):
-    pass  # Define a custom error class based on NameError
-
+def raise_exception_msg(message=""):
+    raise NameError(message)
