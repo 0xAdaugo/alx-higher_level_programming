2ba399a630880eed00d63dcbfb18e2d8f6988d9f
0xAdaugo <onuecheghekwuadaugo@gmail.com>
2023-12-01 16:08:03 -0800

    fixing

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,17 +1,21 @@
 #!/usr/bin/python3
 """
-Python script that takes in a URL, sends a request, and displays
+Python script that takes in a URL, sends a request and displays
 the value of the X-Request-Id variable found in the header
 """
 import urllib.request
 import sys
 
 if __name__ == "__main__":
-    url = sys.argv[1] if len(sys.argv) > 1 else input("Enter URL: ")
-    with urllib.request.urlopen(url) as response:
-        request_id = response.getheader('X-Request-Id')
-        if request_id:
-            print(f"The value of X-Request-Id: {request_id}")
-        else:
-            print("X-Request-Id not found in the header")
+    if len(sys.argv) > 1:
+        url = sys.argv[1]
+        req = urllib.request.Request(url)
+        with urllib.request.urlopen(req) as response:
+            request_id = response.getheader('X-Request-Id')
+            if request_id:
+                print(f"The value of X-Request-Id: {request_id}")
+            else:
+                print("X-Request-Id not found in the header")
+    else:
+        print("Please provide a URL as an argument")
 
